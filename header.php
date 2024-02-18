86a2cd95017edd38989cc35e5e591cdd00e98050
Salini Stephanie <pika2a@hotmail.fr>
2024-02-18 23:30:40 +0100

    Réglage des derniers details sur la partie responsive du footer et modifications des fichiers pour charger les polices localement

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Photograohe Event</title>
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . './assets/images/appareil-photo.png' ; ?> ">
     <?php wp_head(); ?>
 </head>
 
